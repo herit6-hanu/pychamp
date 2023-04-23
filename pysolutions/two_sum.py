57ed5f347e89f6612956030371f7a93c1ea8b1a1
Hanumantha Moola <E007848@cigniti.com>
2023-04-23 23:07:36 +0530

    update the changes

--- pysolutions/two_sum.py
+++ pysolutions/two_sum.py
@@ -1,11 +1,13 @@
-def twoSum(nums,target):
-        indices = []
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    indices.append(i)
-                    indices.append(j)
-                    return indices
-list=[1,2,43,5,453,54,354,3,23,23,3,23,54,3]
-sum=6
-print(twoSum(list,sum))
+def two_sum(nums, target):
+    indices = []
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                indices.append(i)
+                indices.append(j)
+                return indices
+
+
+num_list = [1, 2, 43, 5, 453, 54, 354, 3, 23, 23, 3, 23, 54, 3]
+k = 6
+print(two_sum(num_list, k))
